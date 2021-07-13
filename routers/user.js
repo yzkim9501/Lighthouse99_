0eb258d71ea22f154ca6f8f4249a644cff87e731
Parkjinhong <duabck@naver.com>
2021-07-13 11:38:11 +0900

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -58,6 +58,7 @@ router.post("/register",async(req,res)=>{
 router.post("/login", async(req, res)=>{
     const{email, password} = req.body;
     const user = await User.findOne({email, password}).exec();
+   
 
     if(!user){
         res.send({
