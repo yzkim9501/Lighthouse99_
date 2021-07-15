20e135cce57ac9840fa1a6b1c3786f71bd9b178a
Parkjinhong <duabck@naver.com>
2021-07-15 18:19:47 +0900

    cors 도전 2

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ connect()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://lighthouse99.club.s3-website.ap-northeast-2.amazonaws.com",
+  credentials: true
+}
+app.use(cors(corsOptions));
 
 const boardRouter = require("./routers/board");
 const studyRouter = require("./routers/study");
